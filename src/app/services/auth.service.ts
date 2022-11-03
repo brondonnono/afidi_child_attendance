3fb6f781689de3be584a86b1b888169586c3ca92
Brondon Nono <brondonnono3@gmail.com>
2022-11-03 06:26:23 +0100

    integration of firebase functionnality in the service authService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,19 @@
 import { Injectable } from '@angular/core';
 import { NavigationService } from './navigation.service';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  linkWithPopup,
+  signInWithPopup,
+} from '@angular/fire/auth';
+
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
 
 @Injectable({
   providedIn: 'root'
@@ -7,23 +21,77 @@ import { NavigationService } from './navigation.service';
 export class AuthService {
 
   constructor(
-    private navigationService: NavigationService
+    private auth: Auth
   ) { }
 
-  login({tel , password}) {
-    return null;
-    // this.checkUserType();
-  }
-
-  register() {
-    this.navigationService.goto('login');
-  }
-
-  checkUserType() {
-    return this.navigationService.goto('index');
-  }
-
-  logout() { }
   
+  async login({ email, password }) {
+    try {
+      const user = await signInWithEmailAndPassword(
+        this.auth,
+        email,
+        password
+      );
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async register({ email, password }) {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        this.auth,
+        email,
+        password
+      );
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  logout() {
+    return signOut(this.auth);
+  }
+
+  checkUserType() { }
+
+  deleteUser() {
+    return this.auth.currentUser.delete();
+  }
+
+  async resetPasswordTel(email: string) {
+    return sendPasswordResetEmail(this.auth, email);
+  }
+
+  isconnected() {
+    return (this.auth.currentUser != null);
+  }
+
+  googleAuth() {
+    signInWithPopup(this.auth, googleProvider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      });
+
+    return signInWithPopup(this.auth, googleProvider);
+  }
+
+  externalConnection(provider) {
+    return linkWithPopup(this.auth.currentUser, provider);
+  }
 
 }
