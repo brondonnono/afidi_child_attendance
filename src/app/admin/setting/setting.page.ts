6216bfff3bf5e4be8fcfbe3451a9c428c5020486
Brondon Nono <brondonnono3@gmail.com>
2022-11-04 20:28:08 +0100

    integration of a new police and update of page police started

--- src/app/admin/setting/setting.page.ts
+++ src/app/admin/setting/setting.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-setting',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingPage implements OnInit {
 
-  constructor() { }
+  constructor(
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public changeThemeMode(event): void {
   }
 
 }
